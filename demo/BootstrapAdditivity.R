372b23979b70e15281d141a989b3b23fa3bd54cd
Óscar <430222+Deschain@users.noreply.github.com>
2017-12-15 13:19:35 +0100

    Fixed some demos

--- demo/BootstrapAdditivity.R
+++ demo/BootstrapAdditivity.R
@@ -1,6 +1,7 @@
 library(BML)
-Lung_SM4 <- bml('demo/Data/Lung_SM4',   10, 0.1, 5)
+Lung_SM4 <- bml('demo/Data/Lung_SM4',   10, 0.1, 100)
 
-data <- as.data.frame(t(log(Lung_SM4$bootstrap$EdgeProbabilities[9:(9+3),])))
-boxplot(data, horizontal = TRUE, col = c('red', 'lightblue', 'red','grey'), yaxt='n')
+data <- as.data.frame(t(log(Lung_SM4$bootstrap$EdgeProbabilities[(9+3):9,])))
+boxplot(data, horizontal = TRUE, col = rev(c('red', 'lightblue', 'red','grey')), yaxt='n')
 axis(2, at =1:4, labels = names(data), las = 2)
+title("Log(P(g))")
