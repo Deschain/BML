c26dc49b4134ccef5285e65f62d746b4fc209e89
Óscar <430222+Deschain@users.noreply.github.com>
2018-01-18 20:58:35 +0100

    - Change "constructor" to accept a matrix - Added generic methods for S3 methods - Renaming methods

--- R/BML.R
+++ R/BML.R
@@ -1,16 +1,16 @@
 
-bml <- function(file, ntree, threshold, rep = 0) {
-  data <- read.table(file, header = TRUE, row.names=1, skip = 1)
+bml <- function(data, ntree, threshold, rep = 0) {
   stopifnot(ntree > 0)
   stopifnot(threshold > 0.0, threshold<1.0)
   # Add sanity checks
   bml <- BML(as.matrix(data), ntree, threshold, rep)
   # Maybe store input data aswell
-  class(bml) <- 'bml'
+  class(bml) <- append(class(bml),"bml")
   return(bml)
 }
 
-print.bml <- function(bml){
+
+print.bml <- function(bml) {
   cat("Total Num of Edges ", bml$num_edges, "\n")
   cat("Num of unpruned edges ", bml$num_unpruned_edges, "\n")
   cat("Total number of genes ", bml$num_genes, "\n")
@@ -21,8 +21,17 @@ summary.bml <- function(bml) {
   print(bml)
 }
 
+adjacency_matrix <- function(data) {
+  UseMethod("adjacency_matrix", data)
+}
 
-adjacent_matrix <- function(bml) {
+
+adjacency_matrix.default <- function(data) {
+  print("Adjancency matrix not implemented for class")
+}
+
+
+adjacency_matrix.bml <- function(bml) {
   tmp <- cbind(match(bml$DAG$edges_1, bml$DAG$nodes), match(bml$DAG$edges_2, bml$DAG$nodes))
   adm <- matrix(0, length(bml$DAG$nodes), length(bml$DAG$nodes))
   adm[tmp] <- 1
@@ -31,7 +40,13 @@ adjacent_matrix <- function(bml) {
   return(adm)
 }
 
-write.dot <- function(bml, file = "")
+
+writeDotFile <- function(data, file) {
+  UseMethod("writeDotFile", data, file)
+}
+
+
+writeDotFile.bml <- function(data, file)
 {
-  writeDotFile(bml, file)
+  writeDotFile(data, file)
 }
