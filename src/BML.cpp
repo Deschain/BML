9a4dbeece020f1df2e824e6b53006343f1ca6365
oscar <oscar.rodriguezzalonna@estudiante.uam.es>
2018-01-18 20:52:00 +0100

    Added GPL3 license

--- src/BML.cpp
+++ src/BML.cpp
@@ -1,3 +1,18 @@
+/*
+ BML is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ BML is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with BML. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // [[Rcpp::plugins("cpp11")]]
 
 #include <fstream>
@@ -56,7 +71,6 @@ List BML(IntegerMatrix ma, int ntrees, double pthres, int nrep)
   vector<string> names = as<vector<string> >(colnames(ma));
   vector<SimVar> SimDAG;
 
-  
   List result = StructLearn(data, names, SimDAG, ntrees, pthres);
   
   if(nrep > 0)
